624f9326cfc3a9a82f13b74d494a8ee561c401df
LoganPortscheller <lportsch@purdue.edu>
2025-10-05 18:44:24 -0400

    JavaScript tutorial file addeed and React Native images tutorial

--- Frontend/ReactNativeTutorial/App.js
+++ Frontend/ReactNativeTutorial/App.js
@@ -1,7 +1,12 @@
+/**
+ * Note: This file was written to assist learning while going through a React Native tutorial.
+ * This file is NOT meant to be included in the production of our app.
+ **/
+
 // Initialize the React framework
 import React from "react";
 // Import components from React Native
-import { StypleSheet, Text, SafeAreaView, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 // App function = common root function name
 // export default = let other files see and be able to import function
@@ -14,9 +19,15 @@ export default function App() {
         <SafeAreaView style={styles.container}>
             {/* numberOfLines = max # of lines the text can go on. Once limit is reached, "..." is shown */}
             {/* onPress = makes text act as a link. Pass in function that says what happens when text is clicked */}
-            <Text numberOfLines={1} onPress={handlePress}>
-                Hello World
-            </Text>
+            <Text numberOfLines={1} onPress={textPressed}> Hello World </Text>
+            {/* displaying local image */}
+            <Image source={require('./Frontend/ReactNativeTutorial/Frontend/ReactNativeTutorial/tiger.png')} />
+            {/* When displaying network images, you must specify dimensions */}
+            <Image source = {{ 
+                width: 200,
+                height: 300,
+                uri: "https://picsum.photos/200/300"}} />
+            {/* Note: There are many other properities for resizing images, loading images, etc. */}
         </SafeAreaView>
     );
 }
@@ -27,10 +38,10 @@ const styles = StyleSheet.create({
         // to fill the entire screen
         flex: 1,
         // Can used named colors or RGB colors
-        backgroundColor: "dodgerblue",
+        backgroundColor: "#fff",
         // Allign the items in the container around the center
-        // alignItems: "center",
-        // justifyContent: "center"
+        alignItems: "center",
+        justifyContent: "center"
     }
 });
 
