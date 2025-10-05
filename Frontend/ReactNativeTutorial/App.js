618e886317277e0e4a773d69350545967768efa8
LoganPortscheller <lportsch@purdue.edu>
2025-10-05 16:53:41 -0400

    Text tutorial completed

--- Frontend/ReactNativeTutorial/App.js
+++ Frontend/ReactNativeTutorial/App.js
@@ -6,11 +6,17 @@ import { StypleSheet, Text, SafeAreaView, SafeAreaView } from "react-native";
 // App function = common root function name
 // export default = let other files see and be able to import function
 export default function App() {
+    const textPressed = () => console.log("Text pressed");
+    
     return (
         // View = component that lets you group things
         // SafeAreaView = enures our content is in the "safe area" for iPhones (not covered up by the notch)
-        <SafeAreaView style={styles.container} >
-            <Text> Hello World </Text>
+        <SafeAreaView style={styles.container}>
+            {/* numberOfLines = max # of lines the text can go on. Once limit is reached, "..." is shown */}
+            {/* onPress = makes text act as a link. Pass in function that says what happens when text is clicked */}
+            <Text numberOfLines={1} onPress={handlePress}>
+                Hello World
+            </Text>
         </SafeAreaView>
     );
 }
