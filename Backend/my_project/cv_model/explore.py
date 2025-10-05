1680ffed4b48b32aa58b41bfaf5c66d1a91c2bfe
Pranay Nandkeolyar <pranay.nandkeolyar@gmail.com>
2025-10-05 14:38:42 -0400

    Tested initial model working for object detection

--- Backend/my_project/cv_model/explore.py
+++ Backend/my_project/cv_model/explore.py
@@ -1,16 +1,10 @@
 from ultralytics import YOLO
 import cv2
 
-# Load YOLOv8 model
 model = YOLO("Yolo-Weights/yolov8l.pt")
-
-# Run inference
 results = model("Images/1.png")
 
-# Get image with boxes drawn
 annotated_frame = results[0].plot()
+cv2.imwrite("output.png", annotated_frame)
 
-# Show with OpenCV (this will stay open until 'q' is pressed)
-cv2.imshow("YOLO Detection", annotated_frame)
-cv2.waitKey(0)  # waits indefinitely for a key press
-cv2.destroyAllWindows()
+print("Saved annotated image as output.png")
