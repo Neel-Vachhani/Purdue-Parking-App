177baaf7ac1db418ff6346acb07c8e4fd884c378
Neel-Vachhani <vachhann@purdue.edu>
2025-10-05 16:49:07 -0400

    Added incrementing and decrementing testing to redis_test.py to ensure that newly added availability counters can accurately increment and decrement availability.

--- Backend/redis/redis_test.py
+++ Backend/redis/redis_test.py
@@ -10,8 +10,25 @@ r = redis.Redis(
     password=config('REDIS_PASSWORD'),
 )
 
+# Creating simple key:value pair to test
 success = r.set('foo', 'testing, testing, 123')
 
 result = r.get('foo')
 print(result)
 
+r.delete('foo')
+
+# Test increment and decrement of certain parking key:value pairs. Existence verified using Redis Insights Portal
+
+# PGU starting value
+print(f"Existing Availability of University Street Parking Garage: {r.get('PGU_availability')}")
+print("Incrementing Availability of University Street Parking Garage by 1")
+r.incr('PGU_availability')
+
+# PGU should be incremented by 1.
+print(f"New Availability of University Street Parking Garage: {r.get('PGU_availability')}")
+
+# PGU should be 0 again
+print("Decrementing Availability of University Street Parking Garage by 1")
+r.decr('PGU_availability')
+print(f"Final Availability of University Street Parking Garage: {r.get('PGU_availability')}")
