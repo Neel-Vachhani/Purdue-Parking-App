8507c42f1f2f5b1976cd0c41b2b1457c4fc5a5a1
georgesamra <georgesamra22@gmail.com>
2025-10-07 21:41:00 -0400

    Updated to allow import of ics file along with subscription calendar

--- Backend/my_project/api/services.py
+++ Backend/my_project/api/services.py
@@ -6,19 +6,25 @@ from decouple import config
 import pytz
 
 
-def open_calendar():
-    # filepath = '/Users/georgesamra/Purdue-Parking-App/Backend/my_project/api/calendar_test.ics'
-    # ics_path = Path(filepath)
-    # calendar = icalendar.Calendar.from_ical(ics_path.read_bytes())
-    # for event in calendar.events:
-    # print(event.get("Summary"))
-    # print(event.decoded("DTSTART"))
-    # print(event.decoded("DTEND"))
-    # print(event.get)
-    url = config('GEORGE_CALENDAR_URL')
+def open_file_calendar():
+    filepath = '/Users/georgesamra/Purdue-Parking-App/test.ics'
+    ics_path = Path(filepath)
+    calendar = icalendar.Calendar.from_ical(ics_path.read_bytes())
+    for event in calendar.events:
+        print(event.get("Summary"))
+        print(event.get("Location"))
+        print(event.decoded("DTSTART"))
+        print(event.decoded("DTEND"))
+
+
+def open_subscription_calendar(calendar_url):
+    url = calendar_url
+    url = config('GEORGE_CALENDAR_URL')  # TODO remove before using in prod
     calendar = Calendar(requests.get(url).text)
     for item in sorted(calendar.events, reverse=False):
-        print(item.name, item.begin, item.end, item.location)
+        if "CS" in item.name:
+            print(item.name, item.begin, item.end,
+                  item.location, item.description)
 
 
 def analyze_calendar(ics):
@@ -27,4 +33,5 @@ def analyze_calendar(ics):
 
 
 if __name__ == "__main__":
-    open_calendar()
+    open_file_calendar()
+    # open_subscription_calendar("asf")
