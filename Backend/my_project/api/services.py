f41cb04dc4a198a22e106a1e5a8bc4ff5dac6535
georgesamra <georgesamra22@gmail.com>
2025-10-12 21:40:45 -0400

    Added functionality for recurring ical events

--- Backend/my_project/api/services.py
+++ Backend/my_project/api/services.py
@@ -1,18 +1,23 @@
+import time
+from datetime import datetime, timedelta
 import icalendar
 from ics import Calendar
+import recurring_ical_events
 import requests
 from pathlib import Path
 from decouple import config
 import pytz
+from django.utils import timezone
 
 
 def open_file_calendar():
     filepath = '/Users/georgesamra/Purdue-Parking-App/test.ics'
     ics_path = Path(filepath)
     calendar = icalendar.Calendar.from_ical(ics_path.read_bytes())
-    for event in calendar.events:
+    events = recurring_ical_events.of(calendar).between(pytz.UTC.localize(
+        datetime.now()), pytz.UTC.localize(datetime.now() + timedelta(days=7)))
+    for event in events:
         print(event.get("Summary"))
-        print(event.get("Location"))
         print(event.decoded("DTSTART"))
         print(event.decoded("DTEND"))
 
@@ -22,9 +27,10 @@ def open_subscription_calendar(calendar_url):
     url = config('GEORGE_CALENDAR_URL')  # TODO remove before using in prod
     calendar = Calendar(requests.get(url).text)
     for item in sorted(calendar.events, reverse=False):
-        if "CS" in item.name:
-            print(item.name, item.begin, item.end,
-                  item.location, item.description)
+        # if item.end > pytz.UTC.localize(datetime.now()):
+        print(item)
+    # print(item.name, item.begin, item.end,
+    #      item.location, item.description)
 
 
 def analyze_calendar(ics):
