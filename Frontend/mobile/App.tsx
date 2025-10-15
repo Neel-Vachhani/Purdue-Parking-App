656eefdc232ff152d24372d0d282f3e41b19530f
LoganPortscheller <lportsch@purdue.edu>
2025-10-15 19:35:23 -0400

    Notification permissions and token registration enabled

--- Frontend/mobile/App.tsx
+++ Frontend/mobile/App.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Platform } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
 import ThemeProvider from "./app/theme/ThemeProvider";
 import ThemedView from "./app/components/ThemedView";
 import BottomBar from "./app/components/BottomBar";
@@ -9,24 +14,79 @@ import LoginScreen from "./app/screens/Auth/LogInScreen";
 import SignupScreen from "./app/screens/Auth/SignUpScreen";
 
 type TabKey = "list" | "map" | "settings";
+type AuthModeKey = "signup" | "login" | null;
 
 export default function App() {
-  type AuthModeKey = "signup" | "login" | null;
-  
+  // expoPushToken identifies the device that a push notification would go to
+  const [expoPushToken, setExpoPushToken] = useState<string | null>(null);
   // Default to login screen
   const [authMode, setAuthMode] = React.useState<AuthModeKey>("login");
+  const [tab, setTab] = React.useState<TabKey>("list");
 
+  useEffect(() => {
+    const setupPushNotifications = async () => {
+      // Look for hasLaunched key
+      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
+
+      // Only run this block on the first ever launch of the app on the device
+      if (hasLaunched != "true") {
+        await AsyncStorage.setItem("hasLaunched", "true");
+
+        // Sending notfications to simulators doesn't work
+        if (!Device.isDevice) {
+          return;
+        }
+
+        // Check if we already have permission for push notifications
+        // on iOS, push notifications permissions are controlled by the OS, so apps can remember past preferences when app is reinstalled
+        const { status: currentStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = currentStatus;
+        // If we don't already have permission, ask the user for permission
+        if (currentStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+
+        if (finalStatus !== "granted") {
+          console.log("Push notification permissions denied");
+          return;
+        }
+
+        // Get Expo push token
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log("Expo push token:", token);
+        // save this push token in expoPushToken (local variable)
+        setExpoPushToken(token);
+
+        // Send token to Django backend as token object JSON string
+        await fetch("https://your-django-server.com/api/register-token/", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token: token }),
+        });
+      }
+    };
+
+    setupPushNotifications();
+  }, []);
+
+  // function to render the correct screen based on authMode and tab
   function renderScreen() {
     // If user is not logged in, render the login screen
-    // Once the user is logged in, set isLoggedIn to true
     switch(authMode) {
-      case "login": return (
-        < LoginScreen 
-          onLogin={() => setAuthMode(null) }
-          onRequestSignup={() => setAuthMode("signup")}
-        />
-      );
-      case "signup": return <SignupScreen onSignup={() => { setAuthMode("login"); } } />;
+      case "login": 
+        return (
+          < LoginScreen 
+            onLogin={() => setAuthMode(null) }
+            onRequestSignup={() => setAuthMode("signup")}
+          />
+        );
+      case "signup":
+        return (
+          <SignupScreen
+              onSignup={() => { setAuthMode("login"); } }
+          />
+        );
       case null: // If the user is already logged in, just render the page with tabs
         return (
           <>
@@ -34,11 +94,11 @@ export default function App() {
             <BottomBar active={tab} onChange={setTab} />
           </>
         );
+      
     }
   }
 
-  const [tab, setTab] = React.useState<TabKey>("list");
-
+  // Function to render the correct tab screen
   function renderTab() {
     switch (tab) {
       case "list": return <ParkingListScreen />;
