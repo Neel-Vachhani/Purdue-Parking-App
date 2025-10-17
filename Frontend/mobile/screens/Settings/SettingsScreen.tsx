61dd36799098f59741b892b556b06dede0185424
Utkarsh-M13 <142945737+Utkarsh-M13@users.noreply.github.com>
2025-10-17 01:28:13 -0400

    fix ts bug

--- Frontend/mobile/screens/Settings/SettingsScreen.tsx
+++ Frontend/mobile/screens/Settings/SettingsScreen.tsx
@@ -5,8 +5,6 @@ import * as DocumentPicker from "expo-document-picker";
 import { ThemeContext } from "../../theme/ThemeProvider";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
-import { ThemeContext } from "../../theme/ThemeProvider";
-import * as DocumentPicker from "expo-document-picker";
 
 export default function SettingsScreen() {
   const theme = React.useContext(ThemeContext);
