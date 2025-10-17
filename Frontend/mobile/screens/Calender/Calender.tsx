5b3effabc85cff9e31f357084bb892bb687f2dfc
Pranay Nandkeolyar <pranay.nandkeolyar@gmail.com>
2025-10-17 02:54:31 -0400

    dummy calender ui

---
+++ Frontend/mobile/screens/Calender/Calender.tsx
@@ -0,0 +1,7 @@
+// app/calendar.tsx
+import React from "react";
+import CalendarView from "../../components/CalenderView"
+
+export default function CalendarScreen() {
+  return <CalendarView />;
+}
