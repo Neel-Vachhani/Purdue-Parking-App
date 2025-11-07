28d26b306c11cbeb6b8b639bdb7d35308c64df2c
georgesamra <georgesamra22@gmail.com>
2025-11-06 21:22:40 -0500

    Accidentally started working directly on dev

--- Frontend/mobile/components/NavigationView.tsx
+++ Frontend/mobile/components/NavigationView.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import { View, Text, TextInput, FlatList, StyleSheet } from 'react-native'
-import GooglePlacesTextInput from './map/GooglePlacesInput';
-import StyledGooglePlacesTextInput from './map/GooglePlacesInput';
+import GooglePlacesTextInput from 'react-native-google-places-textinput';
 
 const NavigationView = () => {
   const [places, setPlaces] = React.useState<any[]>([]);
+
+  const customStyles = {
+    container: {
+      marginHorizontal: 16,
+      marginVertical: 8,
+    },
+    input: {
+      backgroundColor: '#fff',
+      borderColor: '#ccc',
+      borderWidth: 1,
+      borderRadius: 10,
+      height: 48,
+      fontSize: 16,
+      color: '#000',
+      paddingHorizontal: 12,
+      elevation: 2,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.1,
+      shadowRadius: 2,
+    },
+    suggestionsContainer: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      marginTop: 4,
+      elevation: 3,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.15,
+      shadowRadius: 4,
+    },
+    suggestionItem: {
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      borderBottomColor: '#eee',
+      borderBottomWidth: 1,
+    },
+    suggestionText: {
+      main: {
+        fontSize: 15,
+        color: '#222',
+      },
+      secondary: {
+        fontSize: 13,
+        color: '#777',
+      },
+    },
+    loadingIndicator: {
+      color: '#999',
+    },
+    placeholder: {
+      color: '#888',
+    },
+    text: {
+      color: '#000',
+    },
+  };
 
   const  handlePlaceSelect = async (place: any) => {
     const response = await fetch('https://places.googleapis.com/v1/places:searchNearby', {
@@ -66,9 +122,26 @@ const NavigationView = () => {
   });
 
 
+
+
   return (
     <View>
-        <GooglePlacesTextInput></GooglePlacesTextInput>
+        <GooglePlacesTextInput
+        apiKey="APIKEY"
+        placeHolderText="Search for a place"
+        onPlaceSelect={handlePlaceSelect}
+        fetchDetails={true}
+        types={['parking']}
+        detailsFields={['formattedAddress', 'location', 'displayName']}
+        style={customStyles}
+        locationRestriction={{
+          rectangle: {
+            low: { latitude: 40.39286, longitude: -86.954622 },
+            high: { latitude: 40.466874, longitude: -86.871755 },
+          },
+        }}>
+
+        </GooglePlacesTextInput>
         {places && places.length > 0 && (
         <NavigationListView places={places} /> )}
     </View>
